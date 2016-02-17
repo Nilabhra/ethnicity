3ded613fd941528c07fc7cd0e9f776f5cb35b943
root <root@instance-1.c.the-broadline.internal>
2016-02-17 10:26:51 +0000

    changed app port

--- app.py
+++ app.py
@@ -72,5 +72,4 @@ def hello_world():
             confidence_ethnicity = results['confidence_ethnicity'].title())
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
